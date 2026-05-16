d81b25622bc7f57485ade643e9000a45f4a29d39
DIMITRIS BEKAS <118062255+DIMPM32145@users.noreply.github.com>
2026-05-16 17:26:57 +0300

    v1.0.1 Implement GameSprite class for paddles
    Added GameSprite class for paddle representation and reset functionality.

--- ping pong game.py
+++ ping pong game.py
@@ -5,10 +5,31 @@ from pygame import *
 window = display.set_mode((800, 600))
 display.set_caption('Ping-Pong game')
 
+#Classes
+# CLASSES
+class GameSprite(sprite.Sprite):
+    def __init__(self, color, player_x, player_y, player_speed, player_width, player_height):
+        super().__init__()
+        self.image = Surface((player_width, player_height))
+        self.image.fill(color)
+        
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 #Variables 
-LIGHT_BLUE = (200, 240, 250)
+LIGHT_BLUE = (200, 240, 250) # Color for the Background
 finish = False
+BLACK = (0, 0, 0) # Color for the Paddles
+
+paddle_left = GameSprite(BLACK, 30, 250, 8, 20, 100)
+paddle_right = GameSprite(BLACK, 750, 250, 8, 20, 100)
+
 
 
 # setting fps
@@ -25,5 +46,9 @@ while game:
     if not finish:
         window.fill(LIGHT_BLUE)
 
+
+        paddle_left.reset()
+        paddle_right.reset()
+        
     display.update()
     clock.tick(FPS)
