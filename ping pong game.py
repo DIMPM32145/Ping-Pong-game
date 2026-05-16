37cd33c22813e4271150d0b277e456aa824fb57d
DIMITRIS BEKAS <118062255+DIMPM32145@users.noreply.github.com>
2026-05-16 17:33:00 +0300

    Implement Player class for paddle movement
    Added Player class to handle paddle movement and updated paddle instances to use the new class.

--- ping pong game.py
+++ ping pong game.py
@@ -22,13 +22,22 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
+class Player(GameSprite):
+    def update(self, up_key, down_key):
+        keys = key.get_pressed()
+        if keys[up_key] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[down_key] and self.rect.y < 500: 
+            self.rect.y += self.speed
+
+
 #Variables 
 LIGHT_BLUE = (200, 240, 250) # Color for the Background
 finish = False
 BLACK = (0, 0, 0) # Color for the Paddles
 
-paddle_left = GameSprite(BLACK, 30, 250, 8, 20, 100)
-paddle_right = GameSprite(BLACK, 750, 250, 8, 20, 100)
+paddle_left = Player(BLACK, 30, 250, 8, 20, 100)
+paddle_right = Player(BLACK, 750, 250, 8, 20, 100)
 
 
 
@@ -46,9 +55,13 @@ while game:
     if not finish:
         window.fill(LIGHT_BLUE)
 
+        paddle_left.update(K_w, K_s)
+        paddle_right.update(K_UP, K_DOWN)
+
+
 
         paddle_left.reset()
         paddle_right.reset()
-        
+
     display.update()
     clock.tick(FPS)
